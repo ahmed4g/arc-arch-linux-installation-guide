28f94fd69d948604b49264a26e7e947977395be0
ahmed4g <ahmed4g@gmail.com>
2022-01-19 20:21:33 +0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -3,21 +3,25 @@
 sudo systemctl set-ntp true
 sudo hwclock --systohc
 
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+
 sudo virsh net-autostart default && sudo virsh net-start default
 
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
-makepkg -si --noconfirm
+# git clone https://aur.archlinux.org/pikaur.git
+# cd pikaur/
+# makepkg -si --noconfirm
 
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git/
 makepkg -si --noconfirm
 
-git clone https://aur.archlinux.org/paru.git
-cd paru/
-makepkg -si --noconfirm
+#git clone https://aur.archlinux.org/paru.git
+#cd paru/
+#makepkg -si --noconfirm
 
-sudo pacman -S xorg gdm gnome gnome-extra figlet packagekit flatpak wget gnome-tweak-tools gnome-shell-extensions chrome-gnome-shell
+sudo pacman -S  gdm gnome gnome-extra figlet packagekit flatpak wget gnome-tweak-tools gnome-shell-extensions chrome-gnome-shell arc-gtk-theme arc-icon-theme
 
 git clone https://aur.archlinux.org/all-repository-fonts.git
 cd all-repository-fonts/
